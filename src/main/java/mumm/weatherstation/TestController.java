ede078203540d75af4617438839e6d05702a0b0f
KristinaMumm <mummkristina@gmail.com>
2026-03-27 21:43:11 +0200

    Initial commit

---
+++ src/main/java/mumm/weatherstation/TestController.java
@@ -0,0 +1,12 @@
+package mumm.weatherstation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/mumm/weatherstation/WeatherstationApplicationTests.java
@@ -0,0 +1,13 @@
+package mumm.weatherstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
