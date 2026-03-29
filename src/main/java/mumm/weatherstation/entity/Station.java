a0ffeb89afa26308d89f5a89a1c8be802bc9a638
KristinaMumm <mummkristina@gmail.com>
2026-03-29 21:53:05 +0300

    add extra data validation on entity level

--- src/main/java/mumm/weatherstation/entity/Station.java
+++ src/main/java/mumm/weatherstation/entity/Station.java
@@ -1,6 +1,10 @@
 package mumm.weatherstation.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "station")
@@ -11,12 +15,19 @@ public class Station {
     @Column(updatable = false)
     private Long id;
 
+    @NotNull(message = "latitude must not be null")
+    @NotBlank(message = "name must not be blank")
     @Column(nullable = false)
     private String name;
 
+    @NotNull(message = "longitude must not be null")
+    @DecimalMin(value = "-90.0", message = "latitude must be >= -90")
+    @DecimalMax(value = "90.0", message = "latitude must be <= 90")
     @Column(nullable = false)
     private Double latitude;
 
+    @DecimalMin(value = "-180.0", message = "longitude must be >= -180")
+    @DecimalMax(value = "180.0", message = "longitude must be <= 180")
     @Column(nullable = false)
     private Double longitude;
 
