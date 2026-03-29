e3f9ece4488f818cf860c8ceb64c9660a62df66c
KristinaMumm <mummkristina@gmail.com>
2026-03-29 17:16:27 +0300

    formatter

--- src/main/java/mumm/weatherstation/controller/WeatherDataController.java
+++ src/main/java/mumm/weatherstation/controller/WeatherDataController.java
@@ -3,7 +3,9 @@ package mumm.weatherstation.controller;
 import mumm.weatherstation.controller.dto.Response;
 import mumm.weatherstation.controller.dto.WeatherDto;
 import mumm.weatherstation.service.WeatherService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
